7ffd1f7e4535b116dd4ca91dc82a12f78722965a
lzcdev <lzc2134@gmail.com>
2018-03-15 11:19:15 +0800

    模块

---
+++ Python进阶/module.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding: utf-8
+
+# __future__
+from __future__ import division
+from __future__ import unicode_literals
+
+print 10 / 3
+
+s = 'am I an unicode'
+print isinstance(s, unicode)
+
+# unicode?
+# 能够使计算机实现跨语言、跨平台的文本转换及处理。Unicode 是「字符集」UTF-8 是「编码规则」
